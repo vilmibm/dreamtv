7bf604d0c59e3556487a85212c84706f122a7bc5
Nate Smith <vilmibm@github.com>
2019-04-12 00:04:39 -0500

    notes

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -6,8 +6,6 @@ import  (
   "path/filepath"
 )
 
-// func init() { }
-
 func insertTape() {
   log.Println("inserting tape")
 
@@ -22,7 +20,17 @@ func insertTape() {
   log.Println(string(out))
 }
 
+func syncChannels(tvdir string, dbfile string) {
+  // goals:
+  // - no video file in db that isn't on disk
+  // - no video file on disk not noted in db
+  // - play count and last played for every file
+  log.Println("scanning", tvdir, "for video files")
+}
+
 func StartScheduler (tvdir string, dbfile string) {
+  syncChannels(tvdir, dbfile)
+
 
   insertTape()
 }
